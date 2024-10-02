22ac2a4144d92d168a82204adab8a2ca2868d4eb
Viktor Persson <viktor.persson@arcsin.se>
2024-10-02 19:13:13 +0200

    Refactor spec.

--- screenshots.spec.ts
+++ screenshots.spec.ts
@@ -1,21 +1,16 @@
 import { expect, test } from "@playwright/test";
 
-import mapSettings from "./map_settings";
+import mapConfig from "./config.maps";
+import config from "./config";
 
-const config = {
-  timeout: 20_000,
-  size: { width: 1024, height: 768 },
-  quality: 90,
-};
-
-for (const [name, pos] of Object.entries(mapSettings)) {
+for (const [name, pos] of Object.entries(mapConfig)) {
   test(name, async ({ page }) => {
     test.setTimeout(config.timeout);
     const fte = page.locator("#fteCanvas");
 
     await test.step("load fte", async () => {
       await page.setViewportSize(config.size);
-      await page.goto("http://localhost:5173");
+      await page.goto(`http://localhost:5173?map=${name}`);
       await expect(page.locator("#fteEngineIsReady")).toBeAttached();
       await page.keyboard.press("Escape", { delay: 100 }); // close main menu
     });
@@ -25,18 +20,19 @@ for (const [name, pos] of Object.entries(mapSettings)) {
       await fte.press("Enter", { delay: 100 });
       await fte.press("Tab", { delay: 100 });
       await fte.press("Enter");
-      await expect(page.locator("#fteMapIsReady")).toBeAttached();
+      await expect(page.locator("#fteMapIsReady")).toBeAttached({
+        timeout: config.mapTimeout,
+      });
     });
 
     await test.step("load pos", async () => {
       await page.keyboard.type(`setpos ${pos}; clear;`);
       await fte.press("Enter", { delay: 100 });
+      await fte.press("~", { delay: 100 }); // hide console
+      await page.waitForTimeout(200); // wait for render
     });
 
-    // screenshot
     await test.step("screenshot", async () => {
-      await fte.press("~", { delay: 100 }); // hide console
-
       await fte.screenshot({
         animations: "disabled",
         path: `./dist/${name}.jpg`,
