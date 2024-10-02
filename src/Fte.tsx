307ba7363384ad8395c2fac607144512df162cec
Viktor Persson <viktor.persson@arcsin.se>
2024-10-02 17:20:26 +0200

    Update assets.

--- src/Fte.tsx
+++ src/Fte.tsx
@@ -1,10 +1,13 @@
-import { getGeneralAssets } from "@/vendor/fte/assets";
+import { getGeneralAssets, getMapAssets } from "@/vendor/fte/assets";
 import { useFteLoader } from "@/vendor/fte/hooks";
 
-const scriptPath = "/ftewebgl_qtv.js";
+// read mapname from map query param
+const scriptPath = "/ftewebgl.js";
 const assets = {
   ...getGeneralAssets(),
+  ...getMapAssets(new URLSearchParams(window.location.search).get("map") || ""),
   "id1/config.cfg": "config.cfg",
+  "qw/qwprogs.qvm": "20240909-210239_2b31159_qwprogs.qvm",
 };
 
 export function FtePlayer() {
