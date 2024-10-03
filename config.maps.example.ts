d9083e16eb6d0e2ca768a1a2aad101cb4ff73c32
Viktor Persson <viktor.persson@arcsin.se>
2024-10-03 17:47:31 +0200

    Update example config.

--- config.maps.example.ts
+++ config.maps.example.ts
@@ -1,4 +1,5 @@
-// see: https://github.com/vikpe/qw-mapshots/blob/main/configs/map_settings.ts
+// for each map define camera position (first triplets) and angle (second triplets)
+// see configs used by qw-mapshots @ https://github.com/vikpe/qw-mapshots/blob/main/configs/
 export default {
   "2bfree": "1380 1067 468 25 194 0",
   "dm3": "1835 -342 18 19 128 0",
